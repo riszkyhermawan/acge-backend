2ecd4006588cab77bbe6585a09eb0c5ab27fc185
Vulbough <71969296+riszkyhermawan@users.noreply.github.com>
2026-01-06 19:51:16 +0700

    (feat) add get submission API endpoint

--- app/submission/crud.py
+++ app/submission/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import desc, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.submission.models import Submission
 from app.submission.schemas import SubmissionCreate
@@ -16,3 +17,16 @@ async def create_submission(db: AsyncSession, submission_data: SubmissionCreate,
     await db.commit()
     await db.refresh(new_submission)
     return new_submission
+
+
+async def get_latest_submission(db: AsyncSession, user_id: int, question_id: int):
+    query = (
+        select(Submission)
+        .where(Submission.user_id == user_id, Submission.question_id == question_id)
+        .order_by(desc(Submission.created_at))
+        .limit(1)
+    )
+    
+    result = await db.execute(query)
+    latest_submission = result.scalars().first()
+    return latest_submission
